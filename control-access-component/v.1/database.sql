b044b0b3063b7eca1d77cefc3bcfa26810b470bb
mdpjulian <mdpjulian@gmail.com>
2022-09-06 21:17:21 +0000

    added line drop/create database

--- control-access-component/v.1/database.sql
+++ control-access-component/v.1/database.sql
@@ -5,6 +5,10 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+DROP DATABASE IF EXISTS `access-control-component`;
+CREATE DATABASE `access-control-component` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+USE `access-control-component`;
+
 DELIMITER ;;
 
 DROP PROCEDURE IF EXISTS `addUserToGroup`;;
